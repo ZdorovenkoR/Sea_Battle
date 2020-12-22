6a0506254c0f8ac6db241d68dd7f260589677598
Roman Zdorovenko <75687097+ZdorovenkoR@users.noreply.github.com>
2020-12-23 00:53:33 +0600

    Update Program.cs

--- Sea_Battle/Program.cs
+++ Sea_Battle/Program.cs
@@ -10,6 +10,30 @@ namespace Sea_Battle
     {
         static void Main(string[] args)
         {
+             string[,] seaBattle =
+            {
+                { "O", "O", "O", "O", "O", "O", "O", "O", "O", "O" },
+                { "O", "X", "X", "X", "X", "O", "X", "X", "O", "O" },
+                { "O", "O", "O", "O", "O", "X", "O", "O", "O", "O" },
+                { "O", "X", "X", "X", "O", "O", "X", "O", "X", "O" },
+                { "O", "O", "O", "O", "X", "O", "X", "O", "O", "O" },
+                { "O", "X", "X", "X", "O", "X", "O", "X", "X", "O" },
+                { "O", "O", "O", "O", "O", "O", "O", "O", "O", "O" },
+                { "O", "O", "O", "X", "O", "O", "O", "O", "X", "O" },
+                { "O", "X", "O", "X", "O", "O", "X", "O", "O", "O" },
+                { "O", "O", "O", "O", "O", "O", "X", "O", "X", "O" },
+                { "O", "O", "O", "O", "O", "O", "O", "O", "O", "O" }
+            };
+            int rows = seaBattle.GetUpperBound(0) + 1;
+            int columns = seaBattle.Length / rows;
+            for (int i=0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                        {
+                        Console.Write($"{seaBattle[i, j]} \t");
+                        }
+                Console.WriteLine();
+                }
         }
     }
 }
